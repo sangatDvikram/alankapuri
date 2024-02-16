af4b295b84d7cb697576149bd427652c64a40119
Vikram Sangat <v.sangat98@gmail.com>
2024-02-17 02:16:15 +0530

    Added postgresql

--- packages/server/settings/settings.py
+++ packages/server/settings/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_DATABASE'),
+        'USER':  env('POSTGRES_USER'),
+        'PASSWORD':  env('POSTGRES_PASSWORD'),
+        'HOST':  env('POSTGRES_HOST'),
+        'PORT':  env('POSTGRES_PORT'),
     }
 }
 
@@ -131,7 +135,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
